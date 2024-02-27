b514d5033cab91430a43c21635a635c79f235b5b
Muahammad Asim <>
2024-02-27 22:31:49 +0100

    [REMOVE] testing

--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -6,18 +6,13 @@ import {educationInfo} from "../../portfolio";
 export default function Education() {
   if (educationInfo.display) {
     return (
-      
       <div className="education-section" id="education">
         <h1 className="education-heading">Education</h1>
-        <div class='sk-ww-linkedin-recommendations' data-embed-id='25372516'></div>
-      <script src='https://widgets.sociablekit.com/linkedin-recommendations/widget.js' async defer></script>
-      
-        {/* 
         <div className="education-card-container">
           {educationInfo.schools.map((school, index) => (
             <EducationCard key={index} school={school} />
           ))}
-        </div> */}
+        </div>
       </div>
     );
   }
